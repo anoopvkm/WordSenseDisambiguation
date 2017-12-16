ecd34fdcc3ae33fdc4f614fe61fd253f0082f7e1
anoopvkm <anoopvkm@gmail.com>
2017-12-16 16:08:37 -0500

    Mappings for word2vec

--- code/datacleaner.py
+++ code/datacleaner.py
@@ -66,16 +66,22 @@ def extract_meta():
         tvocab.add(k)
         sense  = sense.union(vcmap[k])
 
-    
-    vocab = get_words(tvocab)
-    print vocab
-    vmap = {}
-    i = 0
-    for word in vocab:
-        vmap[word] = i
-        i = i + 1
+    words = ['star', 'planet','actor','frequency', 'fish', 'bass','music', 'sound', 'instrument', 'bank', 'finance', 'financial', 'financier', 'institute', 'slope', 'water', 'river']
 
-    json.dump(vmap, open('../vmap.json', 'w'))
+    tvocab = set()
+    sense = set() 
+    for word in words:
+        tvocab.add(word)
+        if word in vcmap:
+            sense = sense.union(vcmap[word])
+
+
+    for word in words:
+        print 'word sense count', word, ct[word]
+
+    vocab = get_words(tvocab)
+    
+    
     smap = {}
     i = 0
     for word in sense:
@@ -83,10 +89,27 @@ def extract_meta():
         i = i + 1
     
     smap['O'] = i
-    json.dump(smap, open('../smap.json', 'w'))
 
-    
-    print len(vocab), len(sense)
+
+    vmap = {}
+    i = 0
+    for word in vocab:
+        if word in words and word in vcmap:
+            for sense in vcmap[word]:
+                tword  = word+str(smap[sense])
+                vmap[tword] = i
+                i = i+1
+
+        else:
+            vmap[word] = i
+            i = i + 1
+
+    json.dump(vmap, open('../vmapw2v.json', 'w'))
+  
+    json.dump(smap, open('../smapw2v.json', 'w'))
+
+    print smap
+    #print len(vocab), len(sense)
 
     return vmap, smap, tvocab
 
@@ -137,7 +160,7 @@ def get_words_sentences(filename,tvocab):
             if word in tvocab:
                 flag = True
         else:
-            print filename, st
+            #print filename, st
             st = [] 
             if flag :
                 vocab = vocab.union(tempvocab)
@@ -149,6 +172,81 @@ def get_words_sentences(filename,tvocab):
                 flag = True
       
     return vocab 
+
+def handle_sentence_word2vec(filename, vmap, smap, tvocab):
+    tree = et.parse(filename)
+    root = tree.getroot()
+
+    sense = []
+    data = []
+    senses = []
+    sentences = [] 
+    prev = 'dummy'
+    flag1 = True
+    flag2 = False
+    st = []
+    for child in root:
+        word =  child.attrib['text']
+        word = word.lower()
+        if child.attrib['break_level'] == 'NO_BREAK' and prev != 'dummy':
+            prev = "not dummy"
+        elif child.attrib['break_level'] == 'NO_BREAK':
+            prev = "not dummy"
+            if word in vmap:
+                data.append(vmap[word])
+                st.append(word)
+            else:
+                flag1 = False
+                continue
+
+            if 'sense' in child.attrib and child.attrib['sense'] in smap:
+                flag2 = True;
+                sense.append(smap[child.attrib['sense']])
+            else:
+                sense.append(smap['O'])
+       
+
+        elif child.attrib['break_level'] == 'SPACE_BREAK':
+            if word in vmap:
+                data.append(vmap[word])
+                st.append(word)
+            else:
+                flag1 = False
+                continue;
+            if 'sense' in child.attrib and child.attrib['sense'] in smap:
+                flag2 = True
+                sense.append(smap[child.attrib['sense']])
+            else:
+                sense.append(smap['O'])
+        else:
+            if flag1 and flag2:
+                senses.append(sense)
+                sentences.append(data)
+                #print filename, st
+            flag1 = True
+            flag2 = False
+            sense = []
+            data = []
+            st = []
+            if word in vmap:
+                data.append(vmap[word])
+                st.append(word)
+            else:
+                flag1 = False
+                continue
+
+            if 'sense' in child.attrib and child.attrib['sense'] in smap:
+                flag2 = True
+                sense.append(smap[child.attrib['sense']])
+            else:
+                sense.append(smap['O'])
+       
+            
+
+        #print len(senses), len(sentences)
+#        print len(senses[0]), len(sentences[0])
+    return (senses, sentences)
+
 
 def handle_sentence(filename, vmap, smap, tvocab):
     tree = et.parse(filename)
@@ -199,7 +297,7 @@ def handle_sentence(filename, vmap, smap, tvocab):
             if flag1 and flag2:
                 senses.append(sense)
                 sentences.append(data)
-                print filename, st
+                #print filename, st
             flag1 = True
             flag2 = False
             sense = []
@@ -227,7 +325,7 @@ def handle_sentence(filename, vmap, smap, tvocab):
 def extract_features():
     
     vmap, smap, tvocab = extract_meta()
-    print 'Extracted meta data'
+    '''print 'Extracted meta data'
     print tvocab
     sl = []
     vl = []
@@ -250,7 +348,8 @@ def extract_features():
     d = json.load(open('../smap.json'))
     print len(a), len(b)
     print len(c), len(d)
-    print c['the']
+    print c['the']'''
+
 
                
 extract_features()
